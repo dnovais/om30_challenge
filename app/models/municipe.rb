8ed18e201d89d841cbca962bd4359b20ddc3255e
Diego Novais <contato@diegonovais.com.br>
2024-03-05 14:50:48 -0300

    feat: add cpf and cns validations

--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -2,4 +2,25 @@ class Municipe < ApplicationRecord
   has_many :addresses
 
   validates :full_name, :cpf, :cns, :email, :birth, :phone, :photo, :status, presence: true
+  validate :cpf_validation, :cns_validation
+
+  private
+
+  def cpf_validation
+    errors.add(:cpf, "is not valid") unless CPF.valid?(cpf)
+  end
+
+  def cns_validation
+    return if cns.blank?
+
+    cns_numbers = cns.gsub(/\D/, '')
+
+    return errors.add(:cns, 'is not valid') unless cns_numbers.length == 15
+
+    sum_numbers = cns_numbers.chars.map.with_index { |char, index| char.to_i * (15 - index) }.sum
+
+    unless sum_numbers % 11 == 0
+      errors.add(:cns, 'is not valid')
+    end
+  end
 end
--- spec/models/municipe_spec.rb
+++ spec/models/municipe_spec.rb
@@ -6,18 +6,82 @@ RSpec.describe Municipe, type: :model do
   end
 
   context 'validations' do
-    context 'municipe' do
-      it 'is not valid without required fields' do
-        required_fields = [:full_name, :cpf, :cns, :email, :birth, :phone, :photo, :status]
-        municipe = Municipe.new
-    
-        required_fields.each do |field|
-          municipe[field] = nil
-          expect(municipe).not_to be_valid
-          expect(municipe.errors[field]).to include("can't be blank")
-          municipe[field] = "some value"
-        end
-      end
+    it 'is valid with valid attributes' do
+      municipe = build(:municipe)
+      expect(municipe).to be_valid
+    end
+
+    it 'is not valid without a full_name' do
+      municipe = build(:municipe, full_name: nil)
+      expect(municipe).not_to be_valid
+      expect(municipe.errors[:full_name]).to include("can't be blank")
+    end
+
+    it 'is not valid without a cpf' do
+      municipe = build(:municipe, cpf: nil)
+      expect(municipe).not_to be_valid
+      expect(municipe.errors[:cpf]).to include("can't be blank")
+    end
+
+    it 'is not valid without a cns' do
+      municipe = build(:municipe, cns: nil)
+      expect(municipe).not_to be_valid
+      expect(municipe.errors[:cns]).to include("can't be blank")
+    end
+
+    it 'is not valid without an email' do
+      municipe = build(:municipe, email: nil)
+      expect(municipe).not_to be_valid
+      expect(municipe.errors[:email]).to include("can't be blank")
+    end
+
+    it 'is not valid without an birth' do
+      municipe = build(:municipe, birth: nil)
+      expect(municipe).not_to be_valid
+      expect(municipe.errors[:birth]).to include("can't be blank")
+    end
+
+    it 'is not valid without an phone' do
+      municipe = build(:municipe, phone: nil)
+      expect(municipe).not_to be_valid
+      expect(municipe.errors[:phone]).to include("can't be blank")
+    end
+
+    it 'is not valid without an photo' do
+      municipe = build(:municipe, photo: nil)
+      expect(municipe).not_to be_valid
+      expect(municipe.errors[:photo]).to include("can't be blank")
+    end
+
+    it 'is not valid without an status' do
+      municipe = build(:municipe, status: nil)
+      expect(municipe).not_to be_valid
+      expect(municipe.errors[:status]).to include("can't be blank")
+    end
+  end
+
+  context 'CNS validation' do
+    it 'is valid with a valid CNS number' do
+      municipe = build(:municipe)
+      expect(municipe).to be_valid
+    end
+
+    it 'is invalid with an invalid CNS number' do
+      municipe = build(:municipe, cns: 'invalid_cns')
+      municipe.valid?
+      expect(municipe.errors[:cns]).to include('is not valid')
+    end
+
+    it 'is invalid with an incorrect length' do
+      municipe = build(:municipe, cns: '123')
+      municipe.valid?
+      expect(municipe.errors[:cns]).to include('is not valid')
+    end
+
+    it 'is invalid with non-numeric characters' do
+      municipe = build(:municipe, cns: 'abcd12345678901')
+      municipe.valid?
+      expect(municipe.errors[:cns]).to include('is not valid')
     end
   end
 end
