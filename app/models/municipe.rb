e5de9cb264134279082da80daba2891f0d39a8dc
Diego Novais <contato@diegonovais.com.br>
2024-03-05 11:54:28 -0300

    feat: test all required fields

--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -1,3 +1,5 @@
 class Municipe < ApplicationRecord
   has_many :addresses
+
+  validates :full_name, :cpf, :cns, :email, :birth, :phone, :photo, :status, presence: true
 end
--- spec/models/municipe_spec.rb
+++ spec/models/municipe_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Municipe, type: :model do
   context 'relationships' do
     it { should have_many(:addresses) }
   end
+
+  context 'validations' do
+    context 'municipe' do
+      it 'is not valid without required fields' do
+        required_fields = [:full_name, :cpf, :cns, :email, :birth, :phone, :photo, :status]
+        municipe = Municipe.new
+    
+        required_fields.each do |field|
+          municipe[field] = nil
+          expect(municipe).not_to be_valid
+          expect(municipe.errors[field]).to include("can't be blank")
+          municipe[field] = "some value"
+        end
+      end
+    end
+  end
 end
