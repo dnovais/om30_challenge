e6ee6599ccc464cf5a1a9d907d6768a9b992ae11
Diego Novais <contato@diegonovais.com.br>
2024-03-05 11:03:37 -0300

    chore: update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_134726) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_135017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.bigint "municipe_id", null: false
+    t.string "street"
+    t.string "complement"
+    t.string "neighborhood"
+    t.string "city"
+    t.string "uf"
+    t.string "ibge_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["municipe_id"], name: "index_addresses_on_municipe_id"
+  end
 
   create_table "municipes", force: :cascade do |t|
     t.string "full_name"
@@ -27,4 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_134726) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "addresses", "municipes"
 end
